8aba4c46c8ed06f9c2530dce5edfb55d7324066e
ltacker <lucas.bertrand.22@gmail.com>
2020-08-21 20:51:54 +0200

    Implement queries

--- client/cli/query.go
+++ client/cli/query.go
@@ -2,15 +2,13 @@ package cli
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/ltacker/poa/x/poa/types"
 )
@@ -28,11 +26,99 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 
 	poaQueryCmd.AddCommand(
 		flags.GetCommands(
-	// TODO: Add query Cmds
+			GetCmdQueryValidator(),
+			GetCmdQueryValidators(),
+			GetCmdQueryParams(),
 		)...,
 	)
 
 	return poaQueryCmd
 }
 
-// TODO: Add Query Commands
+// GetCmdQueryValidator queries information about a validator
+func GetCmdQueryValidator(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "validator [validator-addr]",
+		Short: "Query a validator",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			// Get address
+			addr, err := sdk.ValAddressFromBech32(args[0])
+			if err != nil {
+				return err
+			}
+
+			// We don't want pagination for cli queries
+			params := NewQueryValidatorParams(addr, 0, 100)
+
+			bz, err := clientCtx.JSONMarshaler.MarshalJSON(params)
+			if rest.CheckBadRequestError(w, err) {
+				return
+			}
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryValidator), bz)
+			if err != nil {
+				fmt.Printf("could not resolve %s %s \n", types.QueryValidator, addr)
+				return nil
+			}
+
+			var out types.Validator
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(out)
+		},
+	}
+}
+
+// GetCmdQueryValidators queries all validators
+func GetCmdQueryValidators(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "validators",
+		Short: "Query all validators",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			// We don't want pagination for cli queries
+			params := NewQueryValidatorsParams(0, 100)
+
+			bz, err := clientCtx.JSONMarshaler.MarshalJSON(params)
+			if rest.CheckBadRequestError(w, err) {
+				return
+			}
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryValidators), bz)
+			if err != nil {
+				fmt.Printf("could not resolve %s \n", types.QueryValidators)
+				return nil
+			}
+
+			var out []types.Validator
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(out)
+		},
+	}
+}
+
+// GetCmdQueryParams queries the params
+func GetCmdQueryParams(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "params",
+		Short: "Querythe params",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryParams), nil)
+			if err != nil {
+				fmt.Printf("could not resolve %s \n", types.QueryParams)
+				return nil
+			}
+
+			var out types.Params
+			cdc.MustUnmarshalJSON(res, &out)
+			return cliCtx.PrintOutput(out)
+		},
+	}
+}
--- keeper/querier.go
+++ keeper/querier.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"fmt"
-
 	abci "github.com/tendermint/tendermint/abci/types"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -16,13 +14,68 @@ import (
 func NewQuerier(k Keeper) sdk.Querier {
 	return func(ctx sdk.Context, path []string, req abci.RequestQuery) ([]byte, error) {
 		switch path[0] {
+		case types.QueryValidators:
+			return queryValidators(ctx, req, k)
+
+		case types.QueryValidator:
+			return queryValidator(ctx, req, k)
+
 		case types.QueryParams:
 			return queryParams(ctx, k)
-			// TODO: Put the modules query routes
+
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown poa query endpoint")
 		}
 	}
+}
+
+func queryValidators(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	var params types.QueryValidatorsParams
+
+	// Unmarschal request
+	err := types.ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+
+	// Get all the validators
+	validators := k.GetAllValidators(ctx)
+
+	start, end := client.Paginate(len(validators), params.Page, params.Limit, int(k.GetParams(ctx).MaxValidators))
+	if start < 0 || end < 0 {
+		validators = []types.Validator{}
+	} else {
+		validators = validators[start:end]
+	}
+
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, validators)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+func queryValidator(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	var params types.QueryValidatorParams
+
+	// Unmarschal request
+	err := types.ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+
+	validator, found := k.GetValidator(ctx, params.ValidatorAddr)
+	if !found {
+		return nil, types.ErrNoValidatorFound
+	}
+
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, validator)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
 }
 
 func queryParams(ctx sdk.Context, k Keeper) ([]byte, error) {
@@ -35,6 +88,3 @@ func queryParams(ctx sdk.Context, k Keeper) ([]byte, error) {
 
 	return res, nil
 }
-
-// TODO: Add the modules query functions
-// They will be similar to the above one: queryParams()
