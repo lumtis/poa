5f2cfd01a0777b33438ae94077e4dacd064f554b
ltacker <lucas.bertrand.22@gmail.com>
2020-08-26 13:59:03 +0200

    Create events

--- types/events.go
+++ types/events.go
@@ -2,5 +2,9 @@ package types
 
 // poa module event types
 const (
+	EventTypeSubmitApplication = "submit_application"
+	EventTypeAppendValidator   = "append_validator"
+
+	AttributeKeyValidator  = "validator"
 	AttributeValueCategory = ModuleName
 )
