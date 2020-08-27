7f2032b5ffb11612b195362f9c56e58f0081a060
ltacker <lucas.bertrand.22@gmail.com>
2020-08-27 16:21:40 +0200

    Add events

--- types/events.go
+++ types/events.go
@@ -2,9 +2,21 @@ package types
 
 // poa module event types
 const (
-	EventTypeSubmitApplication = "submit_application"
-	EventTypeAppendValidator   = "append_validator"
+	EventTypeSubmitApplication   = "submit_application"
+	EventTypeAppendValidator     = "append_validator"
+	EventTypeProposeKick         = "propose_kick"
+	EventTypeKickValidator       = "kick_validator"
+	EventTypeLeaveValidatorSet   = "leave_validator_set"
+	EventTypeApproveApplication  = "approve_application"
+	EventTypeRejectApplication   = "reject_application"
+	EventTypeRejectValidator     = "reject_validator"
+	EventTypeApproveKickProposal = "approve_kick_proposal"
+	EventTypeRejectKickProposal  = "reject_kick_proposal"
+	EventTypeKeepValidator       = "keep_validator"
 
-	AttributeKeyValidator  = "validator"
+	AttributeKeyValidator = "validator"
+	AttributeKeyCandidate = "candidate"
+	AttributeKeyVoter     = "voter"
+
 	AttributeValueCategory = ModuleName
 )
